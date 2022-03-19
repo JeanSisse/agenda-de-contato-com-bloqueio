e4298aa2612ab2e8b1a1ea18a181e819759ef73f
Jean Pierre <tavares1988@live.com>
2022-03-19 07:07:12 -0300

    Projeto: API agenda de contato com bloqueio criado

---
+++ src/test/java/br/com/pierre/agenda/AgendaDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pierre.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDeContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
